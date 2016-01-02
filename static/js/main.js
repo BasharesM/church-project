5ed8903b4aa555d5f296c1b0a01bab9df2719d8a
Michel <michel.bashares@gmail.com>
2016-01-03 00:14:27 +0100

    Slider + smart menu

--- static/js/main.js
+++ static/js/main.js
@@ -1,13 +1,81 @@
 $(document).ready(function () {
 
-    // Gestion des sous menu
-    $('nav li').hover(function () {
-        var subMenu = $(this).find('.sub-nav');
-        subMenu.fadeIn();
+    // Smart Menu
+    $('#main-menu').smartmenus();
 
-        $(this).mouseleave(function () {
-            subMenu.fadeOut();
-        });
+    // OWL Carousel
+    $("#owl-carousel").owlCarousel({
+        items : 1,
+//        itemsCustom : false,
+//        itemsDesktop : [1199,4],
+//        itemsDesktopSmall : [980,3],
+//        itemsTablet: [768,2],
+//        itemsTabletSmall: false,
+//        itemsMobile : [479,1],
+//        singleItem : false,
+//        itemsScaleUp : false,
+
+        //Basic Speeds
+        slideSpeed : 200,
+        paginationSpeed : 800,
+        rewindSpeed : 1000,
+
+        //Autoplay
+        autoPlay : true,
+        stopOnHover : false,
+
+        // Navigation
+//        navigation : true,
+//        navigationText : ["<",">"],
+//        rewindNav : true,
+//        scrollPerPage : false,
+
+        //Pagination
+        pagination : true,
+        paginationNumbers: false,
+
+        // Responsive 
+        responsive: true,
+        responsiveRefreshRate : 200,
+        responsiveBaseWidth: window,
+
+        // CSS Styles
+        baseClass : "owl-carousel",
+        //theme : "owl-theme",
+
+        //Lazy load
+        lazyLoad : false,
+        lazyFollow : true,
+        lazyEffect : "fade",
+
+        //Auto height
+        autoHeight : true,
+
+        //JSON 
+        jsonPath : false, 
+        jsonSuccess : false,
+
+        //Mouse Events
+        dragBeforeAnimFinish : true,
+        mouseDrag : true,
+        touchDrag : true,
+
+        //Transitions
+        transitionStyle : false,
+
+        // Other
+        addClassActive : false,
+
+        //Callbacks
+        beforeUpdate : false,
+        afterUpdate : false,
+        beforeInit: false, 
+        afterInit: false, 
+        beforeMove: false, 
+        afterMove: false,
+        afterAction: false,
+        startDragging : false,
+        afterLazyLoad : false
     });
 
 });
